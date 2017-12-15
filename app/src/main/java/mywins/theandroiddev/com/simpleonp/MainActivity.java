854501516595864615f9e1992d54cc39d1a8aeb2
jcup <jakubpchmiel@gmail.com>
2017-12-15 18:43:11 +0100

    Added Simple Layout

--- app/src/main/java/mywins/theandroiddev/com/simpleonp/MainActivity.java
+++ app/src/main/java/mywins/theandroiddev/com/simpleonp/MainActivity.java
@@ -2,18 +2,32 @@ package mywins.theandroiddev.com.simpleonp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements MainView {
 
     MainPresenter mainPresenter;
+
+    TextView resultIsTv, resultTv;
+    EditText insertExpression;
+    Button calculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        resultIsTv = findViewById(R.id.resultIsTv);
+        resultTv = findViewById(R.id.resultTv);
+        insertExpression = findViewById(R.id.insertExpressionEt);
+        calculate = findViewById(R.id.calculate);
+
+
         mainPresenter = new MainPresenterImpl();
         mainPresenter.setView(this);
+
 
     }
 
